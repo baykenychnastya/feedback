a1d68c5e412556959840e39d650c96fc9274b733
baykenychnastya <baykenych.nastya@gmail.com>
2023-02-19 11:23:34 +0200

    add basic styles

--- feedback-web/src/App.tsx
+++ feedback-web/src/App.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import { CreateFeedbackDto } from './dto/create-feedback.dto';
+import  './App.css'
 
 function App() {
   const [name, setName] = useState('')
@@ -19,7 +20,6 @@ function App() {
           'Content-type': 'application/json; charset=UTF-8',
         },
     })
-   // .then((response) => response.json())
     .then((data) => {
       let data2 = data.json()
       setName('');
@@ -33,31 +33,44 @@ function App() {
 
   return (
     <div className="App">
+      <div className="street-view"></div>
 
-      <input 
-        type="text" 
-        value={name}
-        maxLength={100}
-        onChange={event => setName(event.target.value)}
-        placeholder="Your name*"
-      />
-
-      <input 
-        type="text" 
-        value={email}
-        onChange={event => setEmail(event.target.value)}
-        placeholder="Your email*"
-      />
-
-      <input
-        type="text" 
-        value={message}
-        onChange={event => setMessage(event.target.value)}
-        maxLength={500}
-        placeholder="Your message*"
-      />
-
-      <button onClick={sendFeedback}>Send Message</button>
+      <div className="p-relative">
+        <div className="pink-circle"></div>
+        <div id="pink-eye-left"></div>
+        <div id="pink-eye-right"></div>
+        <div id="pink-mouth"></div>
+      </div>
+      <div className="main">
+        <h1 className="title">Reach out to us!</h1>
+        <form onSubmit={sendFeedback}>
+          <input className="input-name" 
+            type="text" 
+            value={name}
+            maxLength={100}
+            onChange={event => setName(event.target.value)}
+            placeholder="Your name*"
+          />
+          <input className="input-email"
+            type="text" 
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+            placeholder="Your email*"
+          />
+           
+          <input className="input-message"
+            type="text" 
+            value={message}
+            onChange={event => setMessage(event.target.value)}
+            maxLength={500}
+            placeholder="Your message*"
+          />
+          <button onClick={sendFeedback}>Send Message</button>
+        </form>
+      </div>
+      <div className="footer">
+        <p>Footer content goes here.</p>
+      </div>
     </div>
   );
 }
